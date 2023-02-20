82369c388e6718276a2589bdca7372aab26a929a
Michael Leung <michael.leung@harmonicinc.com>
2023-02-20 13:00:57 +0800

    Move refresh metadata logic to lib

--- ClientSideAdTrackingDemo/ClientSideAdTrackingDemo/Views/AssetPlaybackView.swift
+++ ClientSideAdTrackingDemo/ClientSideAdTrackingDemo/Views/AssetPlaybackView.swift
@@ -6,14 +6,9 @@
 //
 
 import SwiftUI
-import Combine
 import AVFoundation
 import HarmonicClientSideAdTracking
 import os
-
-let AD_TRACING_METADATA_FILE_NAME = "metadata"
-let EARLY_FETCH_MS: Double = 5_000
-let METADATA_UPDATE_INTERVAL: TimeInterval = 2
 
 struct AssetPlaybackView: View {
     private static let logger = Logger(
@@ -23,9 +18,6 @@ struct AssetPlaybackView: View {
     
     let asset: AssetItem
     
-    @Environment(\.dismiss)
-    private var dismiss
-    
     @EnvironmentObject
     private var assetProvider: AssetProvider
     
@@ -33,13 +25,7 @@ struct AssetPlaybackView: View {
     private var adTracker = HarmonicAdTracker()
     
     @StateObject
-    private var session = Session()
-    
-    @StateObject
     private var playerVM = PlayerViewModel()
-    
-    @State
-    private var lastDataRange: DataRange?
     
     @State
     private var showError = false
@@ -55,13 +41,8 @@ struct AssetPlaybackView: View {
     private var showDeleteAlert = false
 #endif
     
-    @State
-    private var refreshMetadataTimer = Timer.publish(every: METADATA_UPDATE_INTERVAL, on: .main, in: .common)
-    
-    @State
-    private var connectedTimer: Cancellable?
-    
-    private let decoder = JSONDecoder()
+    @Environment(\.dismiss)
+    private var dismiss
     
     var body: some View {
         Group {
@@ -94,7 +75,6 @@ struct AssetPlaybackView: View {
 #endif
         }
         .environmentObject(adTracker)
-        .environmentObject(session)
         .environmentObject(playerVM)
         .toolbar(content: {
 #if os(tvOS)
@@ -107,7 +87,7 @@ struct AssetPlaybackView: View {
             }
             Button("Load") {
                 Task {
-                    await loadMedia(urlString: session.sessionInfo.mediaUrl)
+                    await adTracker.setMediaUrl(asset.urlString)
                 }
             }
         })
@@ -128,154 +108,21 @@ struct AssetPlaybackView: View {
                 dismiss()
             }
         }
-#endif
         .onAppear {
-#if os(tvOS)
             Task {
-                await loadMedia(urlString: session.sessionInfo.mediaUrl)
+                await adTracker.setMediaUrl(asset.urlString)
             }
-#endif
-            refreshMetadataTimer = Timer.publish(every: METADATA_UPDATE_INTERVAL, on: .main, in: .common)
-            connectedTimer = refreshMetadataTimer.connect()
         }
+#endif
         .onDisappear {
-            connectedTimer?.cancel()
+            Task {
+                await adTracker.stop()
+            }
         }
         .onReceive(asset.$urlString) { url in
-            session.sessionInfo.mediaUrl = url
-        }
-        .onReceive(refreshMetadataTimer) { _ in
             Task {
-                await checkNeedUpdate()
+                await adTracker.setMediaUrl(asset.urlString)
             }
-        }
-    }
-}
-
-extension AssetPlaybackView {
-    private func loadMedia(urlString: String) async {
-        guard !urlString.isEmpty else { return }
-        var manifestUrl, adTrackingMetadataUrl: String
-        do {
-            guard let (_, httpResponse) = try await makeRequestTo(urlString) else {
-                return
-            }
-            
-            if let redirectedUrl = httpResponse.url {
-                manifestUrl = redirectedUrl.absoluteString
-                adTrackingMetadataUrl = rewriteUrlToMetadataUrl(redirectedUrl.absoluteString)
-            } else {
-                manifestUrl = urlString
-                adTrackingMetadataUrl = rewriteUrlToMetadataUrl(urlString)
-            }
-            
-            session.sessionInfo = SessionInfo(localSessionId: Date().ISO8601Format(),
-                                              mediaUrl: urlString,
-                                              manifestUrl: manifestUrl,
-                                              adTrackingMetadataUrl: adTrackingMetadataUrl)
-            
-            _ = await refreshMetadata(urlString: adTrackingMetadataUrl, time: nil)
-        } catch {
-            errorMessage = "Error loading media with URL: \(urlString); Error: \(error)"
-            showError = true
-            Self.logger.error("\(errorMessage, privacy: .public)")
-        }
-    }
-    
-    private func checkNeedUpdate() async {
-        var url = session.sessionInfo.adTrackingMetadataUrl
-        let lastPlayheadTime = await adTracker.getPlayheadTime()
-        Self.logger.trace("Calling refreshMetadata without start; playhead is \(Date(timeIntervalSince1970: lastPlayheadTime/1_000), privacy: .public)")
-        let result = await refreshMetadata(urlString: url, time: lastPlayheadTime)
-        if let lastDataRange = lastDataRange {
-            if !isInRange(time: lastPlayheadTime, range: lastDataRange) && !result {
-                url += "&start=\(Int(lastPlayheadTime))"
-                Self.logger.trace("Calling refreshMetadata with start: \(Date(timeIntervalSince1970: lastPlayheadTime/1_000), privacy: .public) with url: \(url, privacy: .public)")
-                if await !refreshMetadata(urlString: url, time: nil) {
-                    Self.logger.warning("refreshMetadata for url with start: \(url, privacy: .public) returned false.")
-                }
-            }
-        }
-    }
-    
-    private func refreshMetadata(urlString: String, time: Double?) async -> Bool {
-        guard !urlString.isEmpty else { return false }
-        do {
-            guard let (data, _) = try await makeRequestTo(urlString) else {
-                return false
-            }
-            
-            decoder.dateDecodingStrategy = .millisecondsSince1970
-            let adBeacon = try decoder.decode(AdBeacon.self, from: data)
-            
-            let adPodIDs = adBeacon.adBreaks.map { $0.id ?? "nil" }
-            var startDate: Date = .distantPast
-            var endDate: Date = .distantPast
-            
-            if let lastDataRange = adBeacon.dataRange {
-                self.lastDataRange = lastDataRange
-                startDate = Date(timeIntervalSince1970: (lastDataRange.start ?? 0) / 1_000)
-                endDate = Date(timeIntervalSince1970: (lastDataRange.end ?? 0) / 1_000)
-                if !isInRange(time: time, range: lastDataRange) {
-                    let timeDate = Date(timeIntervalSince1970: (time ?? 0) / 1_000)
-                    Self.logger.warning("Invalid metadata (with ad pods: \(adPodIDs, privacy: .public)):  Time (\(timeDate, privacy: .public)) not in range (start: \(startDate, privacy: .public), end: \(endDate, privacy: .public))")
-                    return false
-                }
-            } else {
-                Self.logger.warning("No DataRange returned in metadata.")
-                return false
-            }
-            
-            Self.logger.trace("Going to update \(adBeacon.adBreaks.count) ad pods: \(adPodIDs, privacy: .public) with DataRange: \(startDate) to \(endDate)")
-            await adTracker.updatePods(adBeacon.adBreaks)
-            
-            return true
-        } catch {
-            errorMessage = "Error refreshing metadata with URL: \(urlString); Error: \(error)"
-            showError = true
-            Self.logger.error("\(errorMessage, privacy: .public)")
-            return false
-        }
-    }
-    
-    private func makeRequestTo(_ urlString: String) async throws -> (Data, HTTPURLResponse)? {
-        guard let url = URL(string: urlString) else {
-            errorMessage = "Invalid URL: \(urlString)"
-            showError = true
-            Self.logger.error("\(errorMessage, privacy: .public)")
-            return nil
-        }
-        let (data, response) = try await URLSession.shared.data(from: url)
-        guard let httpResponse = response as? HTTPURLResponse else {
-            errorMessage = "Cannot cast URLResponse as HTTPURLResponse for URL: \(urlString)"
-            showError = true
-            Self.logger.error("\(errorMessage, privacy: .public)")
-            return nil
-        }
-        if !(200...299 ~= httpResponse.statusCode) {
-            errorMessage = "Invalid response status: \(httpResponse.statusCode) for URL: \(urlString)"
-            showError = true
-            Self.logger.error("\(errorMessage, privacy: .public)")
-            return nil
-        }
-        return (data, httpResponse)
-    }
-    
-    private func rewriteUrlToMetadataUrl(_ url: String) -> String {
-        return url.replacingOccurrences(of: "\\/[^\\/?]+(\\??[^\\/]*)$",
-                                        with: "/\(AD_TRACING_METADATA_FILE_NAME)$1",
-                                        options: .regularExpression)
-    }
-    
-    private func isInRange(time: Double?, range: DataRange) -> Bool {
-        if let time = time {
-            if let start = range.start, let end = range.end {
-                return start...(end-EARLY_FETCH_MS) ~= time
-            } else {
-                return false
-            }
-        } else {
-            return true
         }
     }
 }
